cc8bc7084568b84a95167673974c46a0ecea90c7
lijialin <1040591521@qq.com>
2017-05-11 13:47:45 +0800

    compress every pictures in the media by cross.view.compressImage

--- cross/views.py
+++ cross/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.http.response import HttpResponse
 from . import models, serializers
 from rest_framework import viewsets, permissions
-
+from PIL import Image
 
 def home(request):
     return render_to_response('homepage.html')
@@ -24,3 +24,25 @@ class CrossPictureViewSet(viewsets.ModelViewSet):
 def test(request):
     pass
     return HttpResponse("OK")
+
+
+def compressImage(request):
+    picture_list = models.CrossPicture.objects.all()
+    for cp in picture_list:
+        image = Image.open(cp.picture)
+        width = image.width
+        height = image.height
+        rate = 1.0
+        if width >= 2000 or height >= 2000:
+            rate = 0.3
+        elif width >= 1000 or height >= 1000:
+            rate = 0.5
+        elif width >= 500 or height >= 500:
+            rate = 0.9
+        width = int(width * rate)
+        height = int(height * rate)
+        image.thumbnail((width, height), Image.ANTIALIAS)
+        image.save('media/' + str(cp.picture), 'JPEG')
+        cp.save()
+    return HttpResponse('compress finish')
+
