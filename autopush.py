3e21b2c95087e25020707bfbf855e3eda8f98fb9
lijialin <1040591521@qq.com>
2017-08-10 18:57:01 +0800

    auto push at 2017-08-10 18:57:01.380644

--- autopush.py
+++ autopush.py
@@ -3,7 +3,11 @@
 import subprocess
 import datetime
 
-
+'''
+function: auto push to github
+author: lijialin
+2017-08-10
+'''
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())])
 subprocess.call(["git", "push", "github_origin", "master"])
