7a3af5cb698f5368eb335db8bdfc65edd359dd03
lijialin <1040591521@qq.com>
2017-08-10 18:45:37 +0800

    add autopush.py

---
+++ autopush.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+
+import subprocess
+import datetime
+
+subprocess.call(["git", "add", "."])
+subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())])
+subprocess.call(["git", "push"])
+
